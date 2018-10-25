048c08e6df57e29a818ac3a6d30470125234991a
Jake McEldowney <jxm5982@psu.edu>
2018-10-25 20:07:30 +0000

    Added google analytics and sitemap

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, url_for, flash, redirect, request, session
-""""
+from flask import Flask, render_template, url_for, flash, redirect, request, session, make_response
+from datetime import datetime, timedata
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 from pymysql import escape_string as thwart
 import gc
-"""
 
 
 app = Flask(__name__)
-"""
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -18,8 +16,7 @@ def login_required(f):
         else:
             flash("Please Login")
             return redirect(url_for('login page'))
-    return wrap
-"""    
+    return wrap   
 
 # CMS Structure: Title, Path, Message
 APP_CONTENT = {
@@ -84,6 +81,22 @@ def dashboard():
         return render_template("dashboard.html", APP_CONTENT = APP_CONTENT)
     except Exception as e:
         return render_template("500.html", error = e)
+    
+@app.route('/sitemap.xml/', methods =["GET"])
+def sitemap():
+    try:
+        page = []
+        week = (datetime.now() - timedelta(days = 7).date()).isoformat()
+        for rule in app.url_map.iter_rules():
+            if "GET" in rule.methods and len(rule.arguments)==0:
+                page.append(["http://165.227.212.179/"+str(rule.rule),week])
+                
+        sitemap_xml = render_template('sitemap_template.xml', page = page)
+        response = make_response(sitemap.xml)
+        response.headers["Content-Type"] = "application/xml"
+            
+    except: Exception as e:
+            returns(str(e))
     
 @app.route("/logout/")
 @login_required
